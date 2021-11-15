cad039b52e0ec8fb80d0c30668da76d09a2de1bf
ajaysagarkaradi <91310339+ajaysagarkaradi@users.noreply.github.com>
2021-11-15 14:18:17 +0530

    Delete src directory

--- src/components/AddStudent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AddStudent(props) {
-  return <div>Add Students</div>;
-}
-
-export default AddStudent;
--- src/components/AllStudents.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AllStudents(props) {
-  return <div>All Students</div>;
-}
-
-export default AllStudents;
